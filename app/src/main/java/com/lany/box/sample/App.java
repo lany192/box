8538aab24e3d856b87145856f2cd442b3803e863
蓝玉贵 <lany192@qq.com>
2018-05-18 11:49:39 +0800

    添加下载示例

--- app/src/main/java/com/lany/box/sample/App.java
+++ app/src/main/java/com/lany/box/sample/App.java
@@ -1,6 +1,13 @@
 package com.lany.box.sample;
 
 import com.lany.box.BaseApp;
+import com.liulishuo.filedownloader.FileDownloader;
 
 public class App extends BaseApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FileDownloader.setupOnApplicationOnCreate(this);
+    }
 }
--- app/src/main/java/com/lany/box/sample/MainActivity.java
+++ app/src/main/java/com/lany/box/sample/MainActivity.java
@@ -1,8 +1,16 @@
 package com.lany.box.sample;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 
 import com.lany.box.activity.BaseActivity;
+import com.lany.box.interfaces.SimpleFileDownloadListener;
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloader;
+
+import java.io.File;
 
 public class MainActivity extends BaseActivity {
 
@@ -13,6 +21,31 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void init(Bundle savedInstanceState) {
+        findViewById(R.id.download_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + File.separator + "gradle-4.4-all.zip";
+                String fileUrl = "https://downloads.gradle.org/distributions/gradle-4.4-all.zip";
+                FileDownloader.getImpl().create(fileUrl)
+                        .setPath(path)
+                        .setListener(new SimpleFileDownloadListener() {
 
+                            @Override
+                            protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                                Log.i(TAG, "下载进度: " + (soFarBytes / (float) totalBytes * 100) + "%");
+                            }
+
+                            @Override
+                            protected void completed(BaseDownloadTask task) {
+                                Log.i(TAG, "completed: 下载完成" + task.getUrl() + " 目标文件路径：" + task.getTargetFilePath());
+                            }
+
+                            @Override
+                            protected void error(BaseDownloadTask task, Throwable e) {
+                                Log.i(TAG, "error: " + e.getMessage());
+                            }
+                        }).start();
+            }
+        });
     }
 }
