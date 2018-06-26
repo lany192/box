b6d2400b83866acf1b9d749e59c0bc7a1e739c35
蓝玉贵 <lany192@qq.com>
2018-06-26 09:20:30 +0800

    增加具有接口请求能力的代理

---
+++ library/src/main/java/com/lany/box/delegate/ModelDelegate.java
@@ -0,0 +1,28 @@
+package com.lany.box.delegate;
+
+/**
+ * 具有请求调用接口能力的代理
+ *
+ * @param <T> 数据类型
+ * @param <M> app请求接口
+ */
+public abstract class ModelDelegate<T, M> extends ItemDelegate<T> {
+    /**
+     * 请求仓库
+     */
+    private M model;
+
+    public ModelDelegate(T data, M model) {
+        super(data);
+        this.model = model;
+    }
+
+    /**
+     * 获取接口提供器
+     *
+     * @return 接口提供器
+     */
+    public M getModel() {
+        return model;
+    }
+}
