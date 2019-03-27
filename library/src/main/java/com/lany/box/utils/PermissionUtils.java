030de4404d941270c4e825e9d4f57b5b8634ea1f
lyg <lany192@qq.com>
2019-03-27 17:42:20 +0800

    修改权限

--- library/src/main/java/com/lany/box/utils/PermissionUtils.java
+++ library/src/main/java/com/lany/box/utils/PermissionUtils.java
@@ -2,14 +2,14 @@ package com.lany.box.utils;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 
 public class PermissionUtils {
 
     /**
      * 检查权限
      */
-    public static boolean checkPermission(Context context, String permissionName) {
-        PackageManager pm = context.getPackageManager();
-        return PackageManager.PERMISSION_GRANTED == pm.checkPermission(permissionName, context.getPackageName());
+    public static boolean checkPermission(Context context, String permission) {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context, permission);
     }
 }
