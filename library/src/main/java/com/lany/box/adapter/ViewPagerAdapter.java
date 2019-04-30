be51da1d5fa4fa28642956f0accf8f5812290085
lyg <lany192@qq.com>
2019-04-30 09:15:05 +0800

    update

--- library/src/main/java/com/lany/box/adapter/ViewPagerAdapter.java
+++ library/src/main/java/com/lany/box/adapter/ViewPagerAdapter.java
@@ -3,20 +3,29 @@ package com.lany.box.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.text.TextUtils;
 
 import java.util.List;
 
-public class ViewPagerAdapter extends FragmentStatePagerAdapter {
-    private List<Fragment> tabs;
+import lombok.Getter;
+import lombok.Setter;
 
-    public ViewPagerAdapter(FragmentManager fm, List<Fragment> items) {
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    private List<TabItem> tabs;
+
+    public ViewPagerAdapter(FragmentManager fm, List<TabItem> items) {
         super(fm);
         this.tabs = items;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return tabs.get(position).getClass().getSimpleName();
+        String title = tabs.get(position).title;
+        if (TextUtils.isEmpty(title)) {
+            return tabs.get(position).getClass().getSimpleName();
+        } else {
+            return tabs.get(position).title;
+        }
     }
 
     @Override
@@ -26,6 +35,13 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return tabs.get(position);
+        return tabs.get(position).fragment;
+    }
+
+    @Setter
+    @Getter
+    public class TabItem {
+        private String title;
+        private Fragment fragment;
     }
 }
