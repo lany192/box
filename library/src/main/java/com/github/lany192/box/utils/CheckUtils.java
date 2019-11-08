0c9cfe28511ab6519155c935982fada06fd55630
lany192 <lany192@qq.com>
2019-11-08 09:48:37 +0800

    修改格式检测工具

--- library/src/main/java/com/github/lany192/box/utils/CheckUtils.java
+++ library/src/main/java/com/github/lany192/box/utils/CheckUtils.java
@@ -5,6 +5,9 @@ import android.text.TextUtils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * 正则表达式，可参考：https://github.com/klinker24/talon-twitter-holo/blob/master/app/src/main/java/com/klinker/android/twitter/utils/text/Regex.java
+ */
 public class CheckUtils {
     /**
      * 验证手机格式
@@ -12,6 +15,58 @@ public class CheckUtils {
     public static boolean checkPhone(String mobiles) {
         String telRegex = "[1][345678]\\d{9}";
         return !TextUtils.isEmpty(mobiles) && mobiles.matches(telRegex);
+    }
+
+    /**
+     * 是否为web链接格式
+     */
+    public static boolean isWebUrl(String url) {
+        if (TextUtils.isEmpty(url)) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(
+                "((?:(http|https|Http|Https):\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)"
+                        + "\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_"
+                        + "\\.\\+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})?\\@)?)?"
+                        + "((?:(?:[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}\\.)+"   // named host
+                        + "(?:"   // plus top level domain
+                        + "(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])"
+                        + "|(?:biz|b[abdefghijmnorstvwyz])"
+                        + "|(?:cat|com|coop|c[acdfghiklmnoruvxyz])"
+                        + "|d[ejkmoz]"
+                        + "|(?:edu|e[cegrstu])"
+                        + "|f[ijkmor]"
+                        + "|(?:gov|g[abdefghilmnpqrstuwy])"
+                        + "|h[kmnrtu]"
+                        + "|(?:info|int|i[delmnoqrst])"
+                        + "|(?:jobs|j[emop])"
+                        + "|k[eghimnrwyz]"
+                        + "|l[abcikrstuvy]"
+                        + "|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])"
+                        + "|(?:name|net|n[acefgilopruz])"
+                        + "|(?:org|om)"
+                        + "|(?:pro|p[aefghklmnrstwy])"
+                        + "|qa"
+                        + "|r[eouw]"
+                        + "|s[abcdeghijklmnortuvyz]"
+                        + "|(?:tel|travel|t[cdfghjklmnoprtvwz])"
+                        + "|u[agkmsyz]"
+                        + "|v[aceginu]"
+                        + "|w[fs]"
+                        + "|y[etu]"
+                        + "|z[amw]))"
+                        + "|(?:(?:25[0-5]|2[0-4]" // or ip address
+                        + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(?:25[0-5]|2[0-4][0-9]"
+                        + "|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(?:25[0-5]|2[0-4][0-9]|[0-1]"
+                        + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                        + "|[1-9][0-9]|[0-9])))"
+                        + "|\\.\\.\\."
+                        + "(?:\\:\\d{1,5})?)" // plus option port number
+                        + "(\\/(?:(?:[a-zA-Z0-9\\;\\/\\?\\:\\@\\&\\=\\#\\~"  // plus option query params
+                        + "\\-\\.\\+\\!\\*\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?"
+                        + "(?:\\b|$)");
+        Matcher m = pattern.matcher(url);
+        return m.matches();
     }
 
     /**
