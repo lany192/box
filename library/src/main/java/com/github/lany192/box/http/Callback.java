3e406caeb53630797b8dfa949e278d356de722dc
lany192 <lany192@qq.com>
2019-12-19 15:29:34 +0800

    代码调整

--- library/src/main/java/com/github/lany192/box/http/Callback.java
+++ library/src/main/java/com/github/lany192/box/http/Callback.java
@@ -8,13 +8,12 @@ import com.elvishew.xlog.XLog;
 import com.github.lany192.box.event.NetWorkEvent;
 import com.github.lany192.box.utils.JsonUtils;
 import com.github.lany192.box.utils.NetUtils;
-import com.google.gson.reflect.TypeToken;
 
 import org.greenrobot.eventbus.EventBus;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Type;
+import java.lang.reflect.ParameterizedType;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
@@ -22,6 +21,14 @@ import io.reactivex.disposables.Disposable;
 public abstract class Callback<T> implements Observer<String> {
     private final String TAG = this.getClass().getSimpleName();
     private Logger.Builder log = XLog.tag(TAG);
+    private Class<T> clz;
+
+    public Class<T> getClz() {
+        if (clz == null) {
+            clz = (Class<T>) (((ParameterizedType) this.getClass().getGenericSuperclass())).getActualTypeArguments()[0];
+        }
+        return clz;
+    }
 
     @Override
     public void onSubscribe(Disposable disposable) {
@@ -37,11 +44,10 @@ public abstract class Callback<T> implements Observer<String> {
             String msg = jsonObject.getString("msg");
             if (code == 200) {
                 String data = jsonObject.getString("data");
-                if(TextUtils.isEmpty(data)){
+                if (TextUtils.isEmpty(data)) {
                     onSuccess(msg, null);
-                }else{
-                    Type type = new TypeToken<T>() {}.getType();
-                    T bean = JsonUtils.json2object(data, type);
+                } else {
+                    T bean = JsonUtils.json2object(data, getClz());
                     if (bean != null) {
                         onSuccess(msg, bean);
                     } else {
