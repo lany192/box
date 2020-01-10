fd21c987669bbef08c2f437ceb7ebaf531a835c0
lany192 <lany192@qq.com>
2020-01-10 10:05:04 +0800

    代码调整

--- library/src/main/java/com/github/lany192/box/utils/MD5Util.java
+++ library/src/main/java/com/github/lany192/box/utils/MD5Util.java
@@ -9,22 +9,21 @@ public class MD5Util {
     public static String md5(String str) {
         byte[] hash;
         try {
-            hash = MessageDigest.getInstance("MD5").digest(
-                    str.getBytes("UTF-8"));
+            hash = MessageDigest.getInstance("MD5").digest(str.getBytes("UTF-8"));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
-            return null;
+            return "";
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-            return null;
+            return "";
         }
-        StringBuilder hex = new StringBuilder(hash.length * 2);
+        StringBuilder builder = new StringBuilder(hash.length * 2);
         for (byte b : hash) {
             if ((b & 0xFF) < 0x10) {
-                hex.append("0");
+                builder.append("0");
             }
-            hex.append(Integer.toHexString(b & 0xFF));
+            builder.append(Integer.toHexString(b & 0xFF));
         }
-        return hex.toString().toUpperCase();
+        return builder.toString().toUpperCase();
     }
 }
