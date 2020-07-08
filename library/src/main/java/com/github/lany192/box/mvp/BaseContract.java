76b5b01d95a832f563e59d53d95a0f6ab851d3de
DESKTOP-QO0TDI7\Lany <lany92@qq.com>
2020-07-08 22:03:42 +0800

    update

--- library/src/main/java/com/github/lany192/box/mvp/BaseContract.java
+++ library/src/main/java/com/github/lany192/box/mvp/BaseContract.java
@@ -5,6 +5,17 @@ public interface BaseContract {
 
     interface View extends BaseView {
 
+        void onCreate();
+
+        void onStart();
+
+        void onResume();
+
+        void onPause();
+
+        void onStop();
+
+        void onDestroy();
     }
 
     interface Presenter {
