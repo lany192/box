2ef0a563fbac4293ebf296573d03dd7897c98593
lany192 <lany192@qq.com>
2020-07-29 16:04:40 +0800

    update gs

--- library/src/main/java/com/github/lany192/box/dialog/DialogFragment.java
+++ library/src/main/java/com/github/lany192/box/dialog/DialogFragment.java
@@ -11,9 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout.LayoutParams;
 
 import androidx.annotation.IdRes;
-import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -25,107 +25,40 @@ import com.elvishew.xlog.Logger;
 import com.elvishew.xlog.XLog;
 import com.github.lany192.box.R;
 import com.github.lany192.box.event.NetWorkEvent;
+import com.github.lany192.box.mvp.BaseView;
 import com.github.lany192.box.utils.DensityUtils;
+import com.github.lany192.view.StateLayout;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.Objects;
 
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 
-public abstract class DialogFragment extends androidx.fragment.app.DialogFragment {
+public abstract class DialogFragment extends androidx.fragment.app.DialogFragment implements StateLayout.OnRetryListener, BaseView {
     protected final String TAG = this.getClass().getSimpleName();
-    protected View mContentView;
-    protected boolean canceledOnTouchOutside = true;
     protected Logger.Builder log = XLog.tag(TAG);
     protected FragmentActivity self;
+    private StateLayout stateLayout;
+    private Unbinder unbinder;
 
-    @LayoutRes
+    protected boolean canceledOnTouchOutside = true;
+
+    /**
+     * 是否执行过懒加载
+     */
+    private boolean isLazyLoaded;
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     protected abstract int getLayoutId();
 
     protected abstract void init();
-
-    @Override
-    public int getTheme() {
-        if (bottomStyle()) {
-            return R.style.BottomDialogTheme;
-        } else {
-            return super.getTheme();
-        }
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.self = getActivity();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (!EventBus.getDefault().isRegistered(this)) {
-            EventBus.getDefault().register(this);
-        }
-    }
-
-    @Override
-    public void onStop() {
-        if (EventBus.getDefault().isRegistered(this)) {
-            EventBus.getDefault().unregister(this);
-        }
-        super.onStop();
-    }
-
-    @Subscribe
-    public void onEvent(NetWorkEvent event) {
-
-    }
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setCancelable(isCancelable());
-        dialog.setCanceledOnTouchOutside(canceledOnTouchOutside);
-        if (!isCancelable()) {
-            dialog.setOnKeyListener((dialog1, keyCode, event) -> keyCode == KeyEvent.KEYCODE_BACK);
-        }
-        return dialog;
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Window window = Objects.requireNonNull(getDialog()).getWindow();
-        if (window != null) {
-            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        }
-        mContentView = inflater.inflate(getLayoutId(), container, false);
-        ButterKnife.bind(this, mContentView);
-        return mContentView;
-    }
-
-    public <T extends View> T findViewById(@IdRes int id) {
-        return mContentView.findViewById(id);
-    }
-
-    public void setCanceledOnTouchOutside(boolean canceledOnTouchOutside) {
-        this.canceledOnTouchOutside = canceledOnTouchOutside;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Window window = Objects.requireNonNull(getDialog()).getWindow();
-        if (window != null) {
-            window.setLayout(getDialogWidth(), getDialogHeight());
-            if (bottomStyle()) {
-                window.setGravity(Gravity.BOTTOM);
-            }
-        }
-        init();
-    }
 
     /**
      * 是否为底部弹窗
@@ -144,6 +77,142 @@ public abstract class DialogFragment extends androidx.fragment.app.DialogFragmen
         } else {
             return DensityUtils.dp2px(300);
         }
+    }
+
+    @Override
+    public int getTheme() {
+        if (bottomStyle()) {
+            return R.style.BottomDialogTheme;
+        } else {
+            return super.getTheme();
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(isCancelable());
+        dialog.setCanceledOnTouchOutside(canceledOnTouchOutside);
+        if (!isCancelable()) {
+            dialog.setOnKeyListener((dialog1, keyCode, event) -> keyCode == KeyEvent.KEYCODE_BACK);
+        }
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        return dialog;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this);
+        }
+        this.self = getActivity();
+    }
+
+    public void setCanceledOnTouchOutside(boolean canceledOnTouchOutside) {
+        this.canceledOnTouchOutside = canceledOnTouchOutside;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Window window = Objects.requireNonNull(getDialog()).getWindow();
+        if (window != null) {
+            window.setLayout(getDialogWidth(), getDialogHeight());
+            if (bottomStyle()) {
+                window.setGravity(Gravity.BOTTOM);
+            }
+        }
+        //需要在FragmentStatePagerAdapter构造方法中配置
+        if (!isLazyLoaded) {
+            isLazyLoaded = true;
+            init();
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        stateLayout = new StateLayout(self);
+        stateLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        stateLayout.setOnRetryListener(this);
+        stateLayout.addView(inflater.inflate(getLayoutId(), null));
+        unbinder = ButterKnife.bind(this, stateLayout);
+        return stateLayout;
+    }
+
+    public <T extends View> T findViewById(@IdRes int id) {
+        return stateLayout.findViewById(id);
+    }
+
+    @Override
+    public void onDestroy() {
+        log.i(TAG + " onDestroy()");
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+        if (null != unbinder) {
+            unbinder.unbind();
+        }
+        if (compositeDisposable != null && compositeDisposable.isDisposed()) {
+            compositeDisposable.dispose();
+            compositeDisposable = null;
+        }
+        super.onDestroy();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(NetWorkEvent event) {
+        //log.i(" 网络状态发送变化");
+    }
+
+    @Override
+    public void onRetry() {
+
+    }
+
+    @Override
+    public void showEmpty() {
+        stateLayout.showEmpty();
+    }
+
+    @Override
+    public void showEmpty(String msg) {
+        stateLayout.showEmpty(msg);
+    }
+
+    @Override
+    public void showContent() {
+        stateLayout.showContent();
+    }
+
+    @Override
+    public void showNoWifi() {
+        stateLayout.showNetwork();
+    }
+
+    @Override
+    public void showError() {
+        stateLayout.showError();
+    }
+
+    @Override
+    public void showError(String msg) {
+        stateLayout.showError(msg);
+    }
+
+    @Override
+    public void showLoading() {
+        stateLayout.showLoading();
+    }
+
+    protected void manageDisposable(Disposable disposable) {
+        compositeDisposable.add(disposable);
     }
 
     @Override
