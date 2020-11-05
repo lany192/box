2e04ef597d5b4c5d4c657227426b9efac07f85a9
lany <lany192@qq.com>
2020-11-06 00:00:45 +0800

    update

--- library/src/main/java/com/github/lany192/box/utils/MD5Util.java
+++ library/src/main/java/com/github/lany192/box/utils/MD5Util.java
@@ -1,6 +1,10 @@
 package com.github.lany192.box.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -25,5 +29,47 @@ public class MD5Util {
             builder.append(Integer.toHexString(b & 0xFF));
         }
         return builder.toString().toUpperCase();
+    }
+
+
+    /**
+     * 获取单个文件的MD5值。解决首位0被省略问题，解决超大文件问题
+     */
+    public static String md5(File file) {
+        StringBuffer buffer;
+        try {
+            char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+            FileInputStream in = new FileInputStream(file);
+            FileChannel ch = in.getChannel();
+            long fileSize = ch.size();
+            int bufferCount = (int) Math.ceil((double) fileSize / (double) Integer.MAX_VALUE);
+            MappedByteBuffer[] mappedByteBuffers = new MappedByteBuffer[bufferCount];
+            long preLength = 0;
+            long regionSize = Integer.MAX_VALUE;
+            for (int i = 0; i < bufferCount; i++) {
+                if (fileSize - preLength < Integer.MAX_VALUE) {
+                    regionSize = fileSize - preLength;
+                }
+                mappedByteBuffers[i] = ch.map(FileChannel.MapMode.READ_ONLY, preLength, regionSize);
+                preLength += regionSize;
+            }
+            MessageDigest messagedigest = MessageDigest.getInstance("MD5");
+            for (int i = 0; i < bufferCount; i++) {
+                messagedigest.update(mappedByteBuffers[i]);
+            }
+            byte[] bytes = messagedigest.digest();
+            int n = bytes.length;
+            buffer = new StringBuffer(2 * n);
+            for (byte bt : bytes) {
+                char c0 = hexDigits[(bt & 0xf0) >> 4];
+                char c1 = hexDigits[bt & 0xf];
+                buffer.append(c0);
+                buffer.append(c1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+        return buffer.toString();
     }
 }
