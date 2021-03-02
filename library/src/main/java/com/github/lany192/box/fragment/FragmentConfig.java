5cad11f3ad676847ce5f57e9cad53869cf7fdf77
lany192 <lany192@qq.com>
2021-03-02 22:55:34 +0800

    update

--- library/src/main/java/com/github/lany192/box/fragment/FragmentConfig.java
+++ library/src/main/java/com/github/lany192/box/fragment/FragmentConfig.java
@@ -3,11 +3,6 @@ package com.github.lany192.box.fragment;
 import androidx.annotation.ColorRes;
 import androidx.annotation.LayoutRes;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Builder
-@Getter
 public class FragmentConfig {
     /**
      * 布局文件id
@@ -38,5 +33,97 @@ public class FragmentConfig {
      */
     public boolean hasToolbar() {
         return toolBarLayoutId != 0;
+    }
+
+    public int getLayoutId() {
+        return layoutId;
+    }
+
+    public void setLayoutId(int layoutId) {
+        this.layoutId = layoutId;
+    }
+
+    public int getToolBarLayoutId() {
+        return toolBarLayoutId;
+    }
+
+    public void setToolBarLayoutId(int toolBarLayoutId) {
+        this.toolBarLayoutId = toolBarLayoutId;
+    }
+
+    public int getToolbarHeight() {
+        return toolbarHeight;
+    }
+
+    public void setToolbarHeight(int toolbarHeight) {
+        this.toolbarHeight = toolbarHeight;
+    }
+
+    public int getToolbarColor() {
+        return toolbarColor;
+    }
+
+    public void setToolbarColor(int toolbarColor) {
+        this.toolbarColor = toolbarColor;
+    }
+
+    public boolean isCoverStyle() {
+        return coverStyle;
+    }
+
+    public void setCoverStyle(boolean coverStyle) {
+        this.coverStyle = coverStyle;
+    }
+    public static Builder builder() {
+        return Builder.builder();
+    }
+    public static final class Builder {
+        private int layoutId;
+        private int toolBarLayoutId;
+        private int toolbarHeight;
+        private int toolbarColor;
+        private boolean coverStyle;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder layoutId(int layoutId) {
+            this.layoutId = layoutId;
+            return this;
+        }
+
+        public Builder toolBarLayoutId(int toolBarLayoutId) {
+            this.toolBarLayoutId = toolBarLayoutId;
+            return this;
+        }
+
+        public Builder toolbarHeight(int toolbarHeight) {
+            this.toolbarHeight = toolbarHeight;
+            return this;
+        }
+
+        public Builder toolbarColor(int toolbarColor) {
+            this.toolbarColor = toolbarColor;
+            return this;
+        }
+
+        public Builder coverStyle(boolean coverStyle) {
+            this.coverStyle = coverStyle;
+            return this;
+        }
+
+        public FragmentConfig build() {
+            FragmentConfig hello = new FragmentConfig();
+            hello.toolbarColor = this.toolbarColor;
+            hello.toolbarHeight = this.toolbarHeight;
+            hello.toolBarLayoutId = this.toolBarLayoutId;
+            hello.layoutId = this.layoutId;
+            hello.coverStyle = this.coverStyle;
+            return hello;
+        }
     }
 }
