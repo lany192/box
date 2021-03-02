1218b5c3a514e04e55f9a141f50dd7ec78cb409c
lany192 <lany192@qq.com>
2021-03-02 23:04:25 +0800

    update

---
+++ library/src/main/java/com/github/lany192/box/event/NetWorkEvent.kt
@@ -0,0 +1,6 @@
+package com.github.lany192.box.event
+
+/**
+ * 网络事件
+ */
+class NetWorkEvent(var available: Boolean)
--- sample/src/main/java/com/github/lany192/box/sample/bean/Result.java
+++ sample/src/main/java/com/github/lany192/box/sample/bean/Result.java
@@ -1,14 +1,34 @@
 package com.github.lany192.box.sample.bean;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
 public class Result<T> {
     private int code;
     private String msg;
     private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 
     public boolean isSuccess() {
         return code == 200;
