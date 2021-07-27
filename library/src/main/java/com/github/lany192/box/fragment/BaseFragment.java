add8afcf79a7b548cb028940d65f6b491de9bd1f
lany192 <lany192@qq.com>
2021-07-27 15:39:25 +0800

    update

--- library/src/main/java/com/github/lany192/box/fragment/BaseFragment.java
+++ library/src/main/java/com/github/lany192/box/fragment/BaseFragment.java
@@ -109,7 +109,6 @@ public abstract class BaseFragment extends Fragment implements StateLayout.OnRet
         if (getConfig().hasToolbar()) {
             View toolbar = inflater.inflate(getConfig().getToolBarLayoutId() == 0 ? R.layout.toolbar_default : getConfig().getToolBarLayoutId(), null);
             toolbar.setId(R.id.toolbar);
-            toolbar.setBackgroundColor(Color.RED);
             toolbar.setOnTouchListener(new OnDoubleClickListener(view -> onToolbarDoubleClick()));
             toolbar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getConfig().getToolbarHeight() == 0 ? DensityUtils.dp2px(48) : getConfig().getToolbarHeight()));
             ViewUtils.setPaddingSmart(toolbar);
---
+++ sample/src/main/java/com/github/lany192/box/sample/MockUtils.java
@@ -0,0 +1,75 @@
+package com.github.lany192.box.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lyg
+ */
+public class MockUtils {
+
+    public static List<String> getItems(int page) {
+        List<String> images = new ArrayList<>();
+        if (page == 1) {
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/fda6ef1bcc.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/3d7174cee3.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/46ff3ad1f2.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114P3.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113113-52.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113114-53.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113527.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113528-52.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113528-53.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/f07d8a8801.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/ab484a269c.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/42929d3f98.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114P7.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114Q0.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114Q1.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105409.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105415.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105418.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105421.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105431.jpg");
+        } else if (page == 2) {
+            images.add("http://e.hiphotos.baidu.com/image/pic/item/eac4b74543a982265bd540e38782b9014b90ebda.jpg");
+            images.add("http://d.hiphotos.baidu.com/image/pic/item/4b90f603738da977625f2cf7bd51f8198718e3fe.jpg");
+            images.add("https://wx3.sinaimg.cn/bmiddle/0060lm7Tgy1g22fgqr55oj30u00k00um.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22ff9ge8zj30u00s777u.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22fhvz2z5j30k00f0mya.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22ff31rzuj30u00s077j.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22fhjkxdoj30hv0ih3zf.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22fh7l9qhj30u01hc7an.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22fi7pdy6j30u0140n35.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22fhwgxx2j30u0140djp.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22fhw7p7aj30u014041u.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22ff9oa5pj30u0140gpd.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22ffkxeptj30rs0rst9k.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22ff37jsaj30u00l6acv.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22ff3ay1hj30k00plq57.jpg");
+            images.add("https://wx4.sinaimg.cn/bmiddle/0060lm7Tgy1g22fgkxa75j30u00mijwr.jpg");
+            images.add("https://wx2.sinaimg.cn/bmiddle/0060lm7Tgy1g22fidcdp4j30tr1sg7a5.jpg");
+            images.add("https://wx3.sinaimg.cn/bmiddle/0060lm7Tgy1g22fhpsiy1j30u0140dhi.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22fflat9oj30u0140tkl.jpg");
+            images.add("https://wx2.sinaimg.cn/bmiddle/0060lm7Tgy1g22fft14xtj30u0140wqo.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22ffsig5wj30u0140wqn.jpg");
+            images.add("https://wx1.sinaimg.cn/bmiddle/0060lm7Tgy1g22ffs63lhj30u0140gv0.jpg");
+        } else {
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/13944db09d.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/b3dacc5656.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/0a299d5bdc.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/71883dd7fe.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/1e2832a665.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/010e4c81a0.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/4c46903d26.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/3f3a4f3ba3.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/161210/9-161210204631.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/190301/9-1Z301155925.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/1a32fce732.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/170108/9-1F10PR424.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/151014/9-1510141Q214.jpg");
+            images.add("https://img.tupianzj.com/uploads/allimg/151014/9-1510141S623.jpg");
+        }
+        return images;
+    }
+}
