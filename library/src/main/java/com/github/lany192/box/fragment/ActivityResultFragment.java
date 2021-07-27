b420f9da1500917913f17f043d1a55c96a90d103
lany192 <lany192@qq.com>
2021-07-27 15:19:24 +0800

    update

--- library/src/main/java/com/github/lany192/box/fragment/ActivityResultFragment.java
+++ library/src/main/java/com/github/lany192/box/fragment/ActivityResultFragment.java
@@ -2,13 +2,14 @@ package com.github.lany192.box.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -19,26 +20,39 @@ import androidx.fragment.app.FragmentTransaction;
  */
 public class ActivityResultFragment extends Fragment {
     private final String TAG = getClass().getSimpleName();
-    private final ActivityResultLauncher<Intent> activityResultLauncher;
-    private final Intent intent;
+    private Intent intent;
+    private ActivityResultCallback<ActivityResult> resultCallback;
 
-    public ActivityResultFragment(Intent intent, ActivityResultCallback<ActivityResult> resultCallback) {
+    public void setIntent(Intent intent) {
         this.intent = intent;
-        this.activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), resultCallback);
     }
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        this.activityResultLauncher.launch(intent);
+        if (intent != null && resultCallback != null) {
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), resultCallback).launch(intent);
+        } else {
+            Log.e(TAG, "没有配置Intent或者ActivityResultCallback,请检查！");
+        }
     }
 
-    public void start(FragmentManager fragmentManager) {
+    public void start(FragmentActivity activity, ActivityResultCallback<ActivityResult> resultCallback) {
+        this.resultCallback = resultCallback;
+        start(activity.getSupportFragmentManager());
+    }
+
+    public void start(Fragment fragment, ActivityResultCallback<ActivityResult> resultCallback) {
+        this.resultCallback = resultCallback;
+        start(fragment.getParentFragmentManager());
+    }
+
+    private void start(FragmentManager fragmentManager) {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         if (fragmentManager.findFragmentByTag(TAG) != null) {
             transaction.remove(this);
         }
         transaction.add(this, TAG);
-        transaction.commit();
+        transaction.commitAllowingStateLoss();
     }
 }
