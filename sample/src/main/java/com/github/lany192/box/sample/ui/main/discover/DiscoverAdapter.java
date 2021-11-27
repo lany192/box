9cec6094bf8210179eb40efa7500d4070c813e46
lany <lany192@qq.com>
2021-11-27 22:02:40 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/main/discover/DiscoverAdapter.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/main/discover/DiscoverAdapter.java
@@ -9,22 +9,19 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.github.lany192.box.helper.ImageLoader;
 import com.github.lany192.box.sample.R;
-import com.github.lany192.box.sample.bean.Area;
 
 import java.util.List;
 
-public class DiscoverAdapter extends BaseQuickAdapter<Area, BaseViewHolder> {
+public class DiscoverAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
 
-    public DiscoverAdapter(@Nullable List<Area> data) {
+    public DiscoverAdapter(@Nullable List<String> data) {
         super(R.layout.item_area, data);
     }
 
     @Override
-    protected void convert(@NonNull BaseViewHolder holder, Area area) {
-        holder.setText(R.id.item_area_title, area.getName());
-        int count = area.getSubarea() != null ? area.getSubarea().size() : 0;
-        holder.setText(R.id.item_poster_running_time, "下辖" + count + "个区/市");
+    protected void convert(@NonNull BaseViewHolder holder, String area) {
+        holder.setText(R.id.item_area_title, "妹子");
         ImageView imageView = holder.getView(R.id.item_poster_post);
-        ImageLoader.get().show(imageView,"https://drimg02.scbao.com/190602/330471-1Z60214395123.jpg");
+        ImageLoader.get().show(imageView,area);
     }
 }
--- sample/src/main/java/com/github/lany192/box/sample/ui/main/discover/DiscoverViewModel.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/main/discover/DiscoverViewModel.java
@@ -1,7 +1,5 @@
 package com.github.lany192.box.sample.ui.main.discover;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
@@ -9,11 +7,8 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.elvishew.xlog.XLog;
-import com.github.lany192.box.sample.bean.Area;
-import com.github.lany192.box.sample.http.ApiCallback;
-import com.github.lany192.box.sample.http.ApiService;
-import com.hjq.toast.ToastUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -22,13 +17,12 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 
 @HiltViewModel
 public class DiscoverViewModel extends ViewModel implements DefaultLifecycleObserver {
-    private final MutableLiveData<List<Area>> items = new MutableLiveData<>();
+    private final MutableLiveData<List<String>> items = new MutableLiveData<>();
     private final MutableLiveData<Boolean> loading = new MutableLiveData<>(true);
-    private final ApiService apiService;
+
 
     @Inject
-    public DiscoverViewModel(ApiService apiService) {
-        this.apiService = apiService;
+    public DiscoverViewModel() {
         requestCityInfo();
     }
 
@@ -36,27 +30,49 @@ public class DiscoverViewModel extends ViewModel implements DefaultLifecycleObse
         return loading;
     }
 
-    public MutableLiveData<List<Area>> getItems() {
+    public MutableLiveData<List<String>> getItems() {
         return items;
     }
 
     public void requestCityInfo() {
-        Log.i("TAG:", "请求城市数据接口");
         loading.postValue(true);
-        apiService.cityInfo().subscribe(new ApiCallback<List<Area>>() {
-
-            @Override
-            public void onSuccess(String msg, List<Area> areas) {
-                loading.postValue(false);
-                items.postValue(areas);
-            }
-
-            @Override
-            public void onFailure(String msg, int code) {
-                loading.postValue(false);
-                ToastUtils.show(msg);
-            }
-        });
+        List<String> images = new ArrayList<>();
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/fda6ef1bcc.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/3d7174cee3.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/46ff3ad1f2.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114P3.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113113-52.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113114-53.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113527.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113528-52.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/200224/37-200224113528-53.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/f07d8a8801.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/ab484a269c.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/42929d3f98.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114P7.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114Q0.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/151002/9-151002114Q1.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105409.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105415.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105418.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105421.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/160209/9-160209105431.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/13944db09d.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/b3dacc5656.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/0a299d5bdc.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/71883dd7fe.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/1e2832a665.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/010e4c81a0.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/4c46903d26.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202002/9999/3f3a4f3ba3.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/161210/9-161210204631.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/190301/9-1Z301155925.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/202011/9999/1a32fce732.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/170108/9-1F10PR424.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/151014/9-1510141Q214.jpg");
+        images.add("https://img.tupianzj.com/uploads/allimg/151014/9-1510141S623.jpg");
+        loading.postValue(false);
+        items.postValue(images);
     }
 
 
