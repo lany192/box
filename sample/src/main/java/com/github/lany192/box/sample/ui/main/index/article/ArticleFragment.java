80ab840c215f1ea4ae0d456e069ba1afeb961495
lany192 <lany192@qq.com>
2021-12-03 14:34:44 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/main/index/article/ArticleFragment.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/main/index/article/ArticleFragment.java
@@ -1,6 +1,7 @@
 package com.github.lany192.box.sample.ui.main.index.article;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import com.github.lany192.box.fragment.BindingFragment;
 import com.github.lany192.box.sample.databinding.FragmentArticleBinding;
 import com.github.lany192.multitype.adapter.MultiAdapter;
+import com.hjq.toast.ToastUtils;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnRefreshLoadMoreListener;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,21 @@ public class ArticleFragment extends BindingFragment<FragmentArticleBinding> {
         binding.recyclerView.setLayoutManager(layoutManager);
         MultiAdapter adapter = new MultiAdapter(new ArrayList<>());
         binding.recyclerView.setAdapter(adapter);
+        binding.refreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                ToastUtils.show("加载更多");
+                new Handler().postDelayed(refreshLayout::finishLoadMore, 2000);
+            }
+
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                ToastUtils.show("刷新");
+                new Handler().postDelayed(refreshLayout::finishRefresh, 2000);
+            }
+        });
+
+
         viewModel.getItems().observe(this, adapter::setNewInstance);
         viewModel.getLoading().observe(this, loading -> {
             if (loading) {
