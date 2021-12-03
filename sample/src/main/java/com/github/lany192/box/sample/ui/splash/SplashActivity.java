fd92658eb63815ff7e87952b6e63593faf4ad7ea
lany192 <lany192@qq.com>
2021-12-03 10:03:31 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/splash/SplashActivity.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/splash/SplashActivity.java
@@ -3,25 +3,29 @@ package com.github.lany192.box.sample.ui.splash;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 
 import com.github.lany192.box.activity.BindingActivity;
-import com.github.lany192.box.sample.databinding.ActivityLoginBinding;
+import com.github.lany192.box.sample.databinding.ActivitySplashBinding;
 import com.github.lany192.box.sample.ui.main.MainActivity;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
-public class SplashActivity extends BindingActivity<ActivityLoginBinding> {
+public class SplashActivity extends BindingActivity<ActivitySplashBinding> {
     private SplashViewModel viewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = getViewModel(SplashViewModel.class);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        viewModel.getWelcome().observe(this, s -> binding.textView.setText(s));
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 1000);
     }
 }
