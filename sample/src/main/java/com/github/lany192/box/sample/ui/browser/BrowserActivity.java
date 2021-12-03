2eff94a8bab8848993906c13d5dd978c518b231c
lany192 <lany192@qq.com>
2021-12-03 14:19:27 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/browser/BrowserActivity.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/browser/BrowserActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.github.lany192.box.activity.BindingActivity;
+import com.github.lany192.box.sample.R;
 import com.github.lany192.box.sample.databinding.ActivityBrowserBinding;
+import com.gyf.immersionbar.ImmersionBar;
 
 public class BrowserActivity extends BindingActivity<ActivityBrowserBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ImmersionBar.with(this).transparentStatusBar().titleBar(binding.toolbar).init();
+
+        binding.toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+        binding.toolbar.setNavigationOnClickListener(v -> finish());
+
+
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
         binding.webview.loadUrl(url);
