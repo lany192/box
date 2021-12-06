d4f940e493784b79cb2c36039b2bbefb61a42c44
lany <lany192@qq.com>
2021-12-06 21:11:56 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/about/AboutActivity.kt
+++ sample/src/main/java/com/github/lany192/box/sample/ui/about/AboutActivity.kt
@@ -1,23 +1,28 @@
 package com.github.lany192.box.sample.ui.about
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.github.lany192.box.activity.BindingActivity
 import com.github.lany192.box.sample.R
 import com.github.lany192.box.sample.databinding.ActivityAboutBinding
 import com.gyf.immersionbar.ImmersionBar
+import com.hjq.toast.ToastUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AboutActivity : BindingActivity<ActivityAboutBinding>() {
-
-    private var viewModel: AboutViewModel? = null
+    private val viewModel: AboutViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = getViewModel(AboutViewModel::class.java)
         ImmersionBar.with(this).transparentStatusBar().statusBarDarkFont(true).titleBar(binding.toolbar).init()
 
         binding.toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material)
-        binding.toolbar.setNavigationOnClickListener { v -> finish() }
+        binding.toolbar.setNavigationOnClickListener { finish() }
+
+        viewModel.loading.observe(this, Observer { it ->
+            ToastUtils.show(it)
+        })
     }
 }
--- sample/src/main/java/com/github/lany192/box/sample/ui/about/AboutViewModel.kt
+++ sample/src/main/java/com/github/lany192/box/sample/ui/about/AboutViewModel.kt
@@ -1,11 +1,13 @@
 package com.github.lany192.box.sample.ui.about
 
+import androidx.lifecycle.MutableLiveData
 import com.github.lany192.box.mvvm.LifecycleViewModel
+import com.github.lany192.box.sample.bean.Area
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AboutViewModel @Inject constructor() : LifecycleViewModel(){
-
+    private val items = MutableLiveData<List<String>>()
 
 }
