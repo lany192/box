adc117fed5daf8a39e9ae49ab1a352cbda798c45
lany <lany192@qq.com>
2021-12-08 22:47:01 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/browser/BrowserActivity.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/browser/BrowserActivity.java
@@ -11,13 +11,15 @@ import com.gyf.immersionbar.ImmersionBar;
 public class BrowserActivity extends BindingActivity<ActivityBrowserBinding> {
 
     @Override
+    public void initImmersionBar() {
+        ImmersionBar.with(this).transparentStatusBar().titleBar(binding.toolbar).init();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImmersionBar.with(this).transparentStatusBar().titleBar(binding.toolbar).init();
-
         binding.toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
         binding.toolbar.setNavigationOnClickListener(v -> finish());
-
 
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
