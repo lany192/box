7f1dbd12fedde79de3d316ed49468999b74a203f
lany192 <lany192@qq.com>
2021-12-14 14:19:22 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/main/index/article/ArticleFragment.kt
+++ sample/src/main/java/com/github/lany192/box/sample/ui/main/index/article/ArticleFragment.kt
@@ -1,12 +1,12 @@
-package com.github.lany192.box.sample.ui.main.index.article;
+package com.github.lany192.box.sample.ui.main.index.article
 
-import com.github.lany192.arch.items.PageListFragment;
-
-import dagger.hilt.android.AndroidEntryPoint;
+import com.github.lany192.arch.items.PageListFragment
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-public class ArticleFragment extends PageListFragment<ArticleViewModel> {
-    {
-        register(new ArticleBinder());
+class ArticleFragment : PageListFragment<ArticleViewModel>() {
+
+    init {
+        register(ArticleBinder())
     }
 }
