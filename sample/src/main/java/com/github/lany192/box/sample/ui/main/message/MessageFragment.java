c77af09c49d0fed94721e81ac0468ca1c945c0b9
lany192 <lany192@qq.com>
2021-12-14 09:51:51 +0800

    update

--- sample/src/main/java/com/github/lany192/box/sample/ui/main/message/MessageFragment.java
+++ sample/src/main/java/com/github/lany192/box/sample/ui/main/message/MessageFragment.java
@@ -9,8 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.github.lany192.arch.fragment.BindingFragment;
+import com.github.lany192.arch.tab.TabAdapter;
+import com.github.lany192.arch.tab.TabItem;
 import com.github.lany192.box.sample.databinding.FragmentMessageBinding;
+import com.github.lany192.box.sample.ui.main.index.article.ArticleFragment;
+import com.github.lany192.box.sample.ui.main.index.city.CityFragment;
+import com.github.lany192.box.sample.ui.main.index.girl.GirlFragment;
 import com.gyf.immersionbar.ImmersionBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -21,7 +29,7 @@ public class MessageFragment extends BindingFragment<FragmentMessageBinding> {
     @Override
     public void initImmersionBar() {
         ImmersionBar.with(this)
-                .statusBarDarkFont(true, 0.2f)
+                .statusBarDarkFont(true)
                 .navigationBarColor(android.R.color.white)
                 .navigationBarDarkIcon(true)
                 .titleBar(binding.toolbar)
@@ -33,6 +41,15 @@ public class MessageFragment extends BindingFragment<FragmentMessageBinding> {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = super.onCreateView(inflater, container, savedInstanceState);
         viewModel = getFragmentViewModel(MessageViewModel.class);
+
+        List<TabItem> items = new ArrayList<>();
+        items.add(new TabItem("互动", new ArticleFragment()));
+        items.add(new TabItem("系统消息", new CityFragment()));
+        items.add(new TabItem("游戏通知", new GirlFragment()));
+
+        TabAdapter tabAdapter = new TabAdapter(requireActivity(), items);
+        binding.viewpager.setAdapter(tabAdapter);
+        binding.tabLayout.setViewPager2(binding.viewpager, tabAdapter.getTitles());
         return root;
     }
 
