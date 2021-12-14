d5690dae4c9a27af7f19ab60b578a956a8927f2d
lany192 <lany192@qq.com>
2021-12-14 14:49:48 +0800

    update

---
+++ arch/src/main/java/com/github/lany192/arch/items/ViewState.java
@@ -0,0 +1,32 @@
+package com.github.lany192.arch.items;
+
+public interface ViewState {
+    /**
+     * 显示内容
+     */
+    int CONTENT_VIEW = 0;
+    /**
+     * 显示错误
+     */
+    int ERROR_VIEW = 1;
+    /**
+     * 显示空数据
+     */
+    int EMPTY_VIEW = 2;
+    /**
+     * 显示加载
+     */
+    int LOADING_VIEW = 3;
+    /**
+     * 显示无网络
+     */
+    int NETWORK_VIEW = 4;
+    /**
+     * 显示加载对话框
+     */
+    int SHOW_LOADING_DIALOG = 5;
+    /**
+     * 取消加载对话框
+     */
+    int CANCEL_LOADING_DIALOG = 6;
+}
