7df1ced9424ec53a841ff24f62a69c31feb1f05a
lany192 <lany192@qq.com>
2022-04-02 18:08:50 +0800

    update

--- matisse/src/main/java/com/zhihu/matisse/internal/ui/widget/CheckView.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/ui/widget/CheckView.java
@@ -74,7 +74,7 @@ public class CheckView extends View {
         mStrokePaint.setStrokeWidth(STROKE_WIDTH * mDensity);
         TypedArray ta = getContext().getTheme().obtainStyledAttributes(new int[]{R.attr.item_checkCircle_borderColor});
         int defaultColor = ResourcesCompat.getColor(
-                getResources(), R.color.zhihu_item_checkCircle_borderColor,
+                getResources(), R.color.item_checkCircle_borderColor,
                 getContext().getTheme());
         int color = ta.getColor(0, defaultColor);
         ta.recycle();
@@ -185,7 +185,7 @@ public class CheckView extends View {
             TypedArray ta = getContext().getTheme()
                     .obtainStyledAttributes(new int[]{R.attr.item_checkCircle_backgroundColor});
             int defaultColor = ResourcesCompat.getColor(
-                    getResources(), R.color.zhihu_item_checkCircle_backgroundColor,
+                    getResources(), R.color.item_checkCircle_backgroundColor,
                     getContext().getTheme());
             int color = ta.getColor(0, defaultColor);
             ta.recycle();
