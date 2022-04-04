f7a21c42bdeb1551becf10485fc1cb74afb7f5c0
lany192 <lany192@qq.com>
2022-04-04 16:25:14 +0800

    update

--- core/src/main/java/com/github/lany192/utils/CacheUtils.java
+++ core/src/main/java/com/github/lany192/utils/CacheUtils.java
@@ -14,8 +14,12 @@ public class CacheUtils {
      * 获取缓存大小
      */
     public static String getCacheSize(Context context) {
+        long cacheSize = getFolderSize(context.getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            cacheSize += getFolderSize(context.getExternalCacheDir());
+        }
         try {
-            return getFormatSize(getFolderSize(context.getCacheDir()));
+            return getFormatSize(cacheSize);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -25,45 +29,41 @@ public class CacheUtils {
     /**
      * * 清除本应用所有的数据
      */
-    public static void clean(Context context) {
-        //清除沙盒缓存数据 清除本应用内部缓存(/data/data/com.xxx.xxx/cache)
-        deleteFilesByDirectory(context.getCacheDir());
-        //清除外部SD缓存数据 清除外部cache下的内容(/mnt/sdcard/android/data/com.xxx.xxx/cache)
+    public static void cleanCache(Context context) {
+        deleteDir(context.getCacheDir());
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            deleteFilesByDirectory(context.getExternalCacheDir());
+            deleteDir(context.getExternalCacheDir());
         }
-        //清除内部缓存数据 清除本应用所有数据库(/data/data/com.xxx.xxx/databases)
-        deleteFilesByDirectory(new File("/data/data/" + context.getPackageName() + "/databases"));
-        //清除沙盒文件缓存 清除/data/data/com.xxx.xxx/files下的内容
-        deleteFilesByDirectory(context.getFilesDir());
-        //清除本应用SharedPreference(/data/data/com.xxx.xxx/shared_prefs)
-//        deleteFilesByDirectory(new File("/data/data/" + context.getPackageName() + "/shared_prefs"));
     }
 
     /**
-     * * 删除方法 这里只会删除某个文件夹下的文件，如果传入的directory是个文件，将不做处理
+     * 删除文件夹
      */
-    private static void deleteFilesByDirectory(File directory) {
-        if (directory != null && directory.exists() && directory.isDirectory()) {
-            for (File item : directory.listFiles()) {
-                item.delete();
+    private static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            for (String child : dir.list()) {
+                boolean success = deleteDir(new File(dir, child));
+                if (!success) {
+                    return false;
+                }
             }
         }
+        return dir.delete();
     }
 
-    // 获取文件
-    //Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
-    //Context.getExternalCacheDir() --> SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+    /**
+     * 获取文件夹大小
+     */
     private static long getFolderSize(File file) {
         long size = 0;
         try {
             File[] fileList = file.listFiles();
-            for (int i = 0; i < fileList.length; i++) {
+            for (File value : fileList) {
                 // 如果下面还有文件
-                if (fileList[i].isDirectory()) {
-                    size = size + getFolderSize(fileList[i]);
+                if (value.isDirectory()) {
+                    size = size + getFolderSize(value);
                 } else {
-                    size = size + fileList[i].length();
+                    size = size + value.length();
                 }
             }
         } catch (Exception e) {
@@ -78,21 +78,19 @@ public class CacheUtils {
     private static String getFormatSize(double size) {
         double kiloByte = size / 1024;
         if (kiloByte < 1) {
-            return size + " Byte";
+//            return size + " Byte";
+            return "0 KB";
         }
-
         double megaByte = kiloByte / 1024;
         if (megaByte < 1) {
             BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
             return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + " KB";
         }
-
         double gigaByte = megaByte / 1024;
         if (gigaByte < 1) {
             BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
             return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + " MB";
         }
-
         double teraBytes = gigaByte / 1024;
         if (teraBytes < 1) {
             BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
