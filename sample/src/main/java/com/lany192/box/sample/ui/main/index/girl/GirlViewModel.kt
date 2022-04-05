7cf8e9569299e3fc47d977ede9d9cac5524fc25c
lany192 <lany192@qq.com>
2022-04-05 22:20:58 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/main/index/girl/GirlViewModel.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/main/index/girl/GirlViewModel.kt
@@ -1,6 +1,7 @@
 package com.lany192.box.sample.ui.main.index.girl
 
 import android.os.Handler
+import android.os.Looper
 import com.github.lany192.arch.items.ItemsViewModel
 import com.lany192.box.sample.MockUtils
 import com.lany192.box.sample.data.api.ApiService
@@ -12,14 +13,23 @@ class GirlViewModel @Inject constructor(private val apiService: ApiService) : It
     var count = 0
 
     override fun request(refresh: Boolean) {
+        if (refresh) {
+            count = 0
+        }
         if (count < 5) {
             val items: MutableList<Any> = ArrayList()
-            for (i in 0..10) {
+            for (i in 0..5) {
                 items.add(MockUtils.getImageUrl())
             }
-            Handler().postDelayed({
-                resetItems(items)
-                moreLoadFinish()
+            Handler(Looper.getMainLooper()).postDelayed({
+                if (refresh) {
+                    resetItems(items)
+                    refreshFinish()
+                } else {
+                    addItems(items)
+                    moreLoadFinish()
+                }
+                count++
             }, 1000)
         } else {
             moreLoadEnd()
