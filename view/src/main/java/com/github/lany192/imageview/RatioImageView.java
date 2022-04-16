9776bb68e12d104113cbd521921155994bf6bad2
lany192 <lany192@qq.com>
2022-04-16 19:31:08 +0800

    update

--- view/src/main/java/com/github/lany192/imageview/RatioImageView.java
+++ view/src/main/java/com/github/lany192/imageview/RatioImageView.java
@@ -1,15 +1,18 @@
 package com.github.lany192.imageview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
-import androidx.annotation.Nullable;
-
+import com.github.lany192.view.R;
 import com.google.android.material.imageview.ShapeableImageView;
 
 /**
  * 比例图片，以宽为基准
+ * <p>
+ * 1.如果指定了比例，显示指定比例
+ * 2.如果没有指定比例，自适应图片比例
  */
 public class RatioImageView extends ShapeableImageView {
     /**
@@ -18,15 +21,20 @@ public class RatioImageView extends ShapeableImageView {
     private float ratio;
 
     public RatioImageView(Context context) {
-        super(context);
+        this(context, null, 0);
     }
 
-    public RatioImageView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+    public RatioImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
     }
 
-    public RatioImageView(Context context, @Nullable AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public RatioImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.RatioImageView, 0, 0);
+            ratio = array.getFloat(R.styleable.RatioImageView_riv_ratio, 0.0f);
+            array.recycle();
+        }
     }
 
     /**
@@ -40,16 +48,19 @@ public class RatioImageView extends ShapeableImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         if (ratio > 0) {
+            //显示指定宽高比
             int width = MeasureSpec.getSize(widthMeasureSpec);
             int height = (int) (width / ratio);
             setMeasuredDimension(width, height);
         } else {
             Drawable drawable = getDrawable();
             if (drawable != null) {
+                //自适应原始图片比
                 int width = MeasureSpec.getSize(widthMeasureSpec);
                 int height = (int) (width * drawable.getIntrinsicHeight() * 1f / drawable.getIntrinsicWidth());
                 setMeasuredDimension(width, height);
             } else {
+                //不处理，使用默认配置
                 super.onMeasure(widthMeasureSpec, heightMeasureSpec);
             }
         }
