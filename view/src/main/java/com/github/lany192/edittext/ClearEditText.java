d685e4a27b0c32de0a670781c65e48ab93899ffe
lany192 <lany192@qq.com>
2022-05-29 18:06:49 +0800

    update

--- view/src/main/java/com/github/lany192/edittext/ClearEditText.java
+++ view/src/main/java/com/github/lany192/edittext/ClearEditText.java
@@ -25,39 +25,33 @@ import com.github.lany192.view.R;
  * 带删除功能的EditText
  */
 public class ClearEditText extends BoxEditText implements OnFocusChangeListener, TextWatcher {
-    private Drawable mLeftDrawable;
+    private Drawable mLeftIcon;
     private boolean hasFocus;
     @DrawableRes
-    private int mLeftDrawableResId;
+    private int mLeftIconResId;
     private boolean mClearEnable = false;
     @DrawableRes
-    private int mClearDrawableResId = R.drawable.ico_input_clear;
+    private int mClearIconResId = R.drawable.ico_input_clear;
 
-    private float mClearDrawableSize = dp2px(18);
-    private float mLeftDrawableSize = dp2px(18);
+    private float mClearIconSize = dp2px(18);
+    private float mLeftIconSize = dp2px(18);
 
     public ClearEditText(Context context) {
-        super(context);
-        init(null);
+        this(context, null);
     }
 
     public ClearEditText(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(attrs);
+        this(context, attrs, 0);
     }
 
     public ClearEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs);
-    }
-
-    private void init(AttributeSet attrs) {
         if (attrs != null) {
             TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.ClearEditText);
-            mClearDrawableResId = a.getResourceId(R.styleable.ClearEditText_clear_icon, mClearDrawableResId);
-            mClearDrawableSize = a.getDimension(R.styleable.ClearEditText_clear_size, dp2px(18));
-            mLeftDrawableResId = a.getResourceId(R.styleable.ClearEditText_left_icon, -1);
-            mLeftDrawableSize = a.getDimension(R.styleable.ClearEditText_left_size, dp2px(18));
+            mClearIconResId = a.getResourceId(R.styleable.ClearEditText_clear_icon, mClearIconResId);
+            mClearIconSize = a.getDimension(R.styleable.ClearEditText_clear_size, dp2px(18));
+            mLeftIconResId = a.getResourceId(R.styleable.ClearEditText_left_icon, -1);
+            mLeftIconSize = a.getDimension(R.styleable.ClearEditText_left_size, dp2px(18));
             a.recycle();
         }
         initClearIcon();
@@ -71,30 +65,30 @@ public class ClearEditText extends BoxEditText implements OnFocusChangeListener,
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, Resources.getSystem().getDisplayMetrics());
     }
 
-    public void setClearDrawable(@DrawableRes int resId) {
-        this.mClearDrawableResId = resId;
+    public void setClearIcon(@DrawableRes int resId) {
+        this.mClearIconResId = resId;
         invalidate();
     }
 
     private void initClearIcon() {
-        Drawable clearDrawable = getCompoundDrawables()[2];
-        if (clearDrawable == null) {
-            clearDrawable = ContextCompat.getDrawable(getContext(), mClearDrawableResId);
+        Drawable ClearIcon = getCompoundDrawables()[2];
+        if (ClearIcon == null) {
+            ClearIcon = ContextCompat.getDrawable(getContext(), mClearIconResId);
         }
-        clearDrawable.setBounds(0, 0, (int) mClearDrawableSize, (int) mClearDrawableSize);
-        Drawable right = mClearEnable ? clearDrawable : null;
+        ClearIcon.setBounds(0, 0, (int) mClearIconSize, (int) mClearIconSize);
+        Drawable right = mClearEnable ? ClearIcon : null;
         setCompoundDrawables(getCompoundDrawables()[0], getCompoundDrawables()[1], right, getCompoundDrawables()[3]);
     }
 
     private void initLeftIcon() {
-        if (mLeftDrawableResId > 0) {
-            mLeftDrawable = getCompoundDrawables()[0];
-            if (mLeftDrawable == null) {
-                mLeftDrawable = ContextCompat.getDrawable(getContext(), mLeftDrawableResId);
+        if (mLeftIconResId > 0) {
+            mLeftIcon = getCompoundDrawables()[0];
+            if (mLeftIcon == null) {
+                mLeftIcon = ContextCompat.getDrawable(getContext(), mLeftIconResId);
             }
-            mLeftDrawable.setBounds(0, 0, (int) mLeftDrawableSize, (int) mLeftDrawableSize);
+            mLeftIcon.setBounds(0, 0, (int) mLeftIconSize, (int) mLeftIconSize);
         }
-        Drawable left = mLeftDrawable != null ? mLeftDrawable : null;
+        Drawable left = mLeftIcon != null ? mLeftIcon : null;
         setCompoundDrawables(left, getCompoundDrawables()[1], getCompoundDrawables()[2], getCompoundDrawables()[3]);
     }
 
@@ -123,7 +117,7 @@ public class ClearEditText extends BoxEditText implements OnFocusChangeListener,
     }
 
     public void setLeftIconResource(@DrawableRes int resId) {
-        mLeftDrawableResId = resId;
+        mLeftIconResId = resId;
         initLeftIcon();
     }
 
