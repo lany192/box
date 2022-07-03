0770dbd4d7fb66ebf8762671ad9564d92033fd31
lany192 <lany192@qq.com>
2022-07-03 14:22:41 +0800

    update

---
+++ view/src/main/java/com/github/lany192/text/html/ClickableTableSpan.java
@@ -0,0 +1,24 @@
+package com.github.lany192.text.html;
+
+import android.text.style.ClickableSpan;
+
+/**
+ * This span defines what should happen if a table is clicked. This abstract class is defined so
+ * that applications can access the raw table HTML and do whatever they'd like to render it (e.g.
+ * show it in a WebView).
+ */
+public abstract class ClickableTableSpan extends ClickableSpan {
+    protected String tableHtml;
+
+    // This sucks, but we need this so that each table can get its own ClickableTableSpan.
+    // Otherwise, we end up removing the clicking from earlier tables.
+    public abstract ClickableTableSpan newInstance();
+
+    public String getTableHtml() {
+        return tableHtml;
+    }
+
+    public void setTableHtml(String tableHtml) {
+        this.tableHtml = tableHtml;
+    }
+}
