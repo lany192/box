50e9ea70c32dc4dd0b0be513aac320b9e6bb3fe4
lany192 <lany192@qq.com>
2022-07-24 20:01:18 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/main/my/MyFragment.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/main/my/MyFragment.kt
@@ -1,5 +1,8 @@
 package com.lany192.box.sample.ui.main.my
 
+import android.content.pm.ActivityInfo
+import androidx.activity.result.ActivityResult
+import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.AppRouter
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.github.lany192.arch.fragment.VMVBFragment
@@ -7,15 +10,20 @@ import com.github.lany192.dialog.BirthdayDialog
 import com.github.lany192.dialog.MenuDialog
 import com.github.lany192.dialog.SimpleDialog
 import com.github.lany192.extensions.load
+import com.github.lany192.utils.DensityUtils
 import com.gyf.immersionbar.ImmersionBar
 import com.hjq.toast.ToastUtils
+import com.lany192.box.sample.BuildConfig
 import com.lany192.box.sample.R
 import com.lany192.box.sample.data.bean.UserInfo
 import com.lany192.box.sample.databinding.FragmentMyBinding
 import com.lany192.box.sample.ui.user.UserViewModel
+import com.zhihu.matisse.GlideEngine
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.internal.entity.CaptureStrategy
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.LocalDate
-import java.util.*
 
 @AndroidEntryPoint
 @Route(path = "/page/my")
@@ -38,7 +46,7 @@ class MyFragment : VMVBFragment<MyViewModel, FragmentMyBinding>() {
         binding.helloView.setOnClickListener { AppRouter.startHello() }
         binding.dialog2View.setOnClickListener { showDialog2() }
         binding.birthday.setOnClickListener {
-            BirthdayDialog(LocalDate.of(2001,1,2)).show()
+            BirthdayDialog(LocalDate.of(2001, 1, 2)).show()
         }
         binding.testView.setOnClickListener {
             userViewModel.setName("我是张三")
@@ -47,6 +55,22 @@ class MyFragment : VMVBFragment<MyViewModel, FragmentMyBinding>() {
 
         binding.checkView.setOnCheckChangeListener { ToastUtils.show(it) }
         binding.checkView.isChecked = true
+        binding.imagePicker.setOnClickListener {
+            Matisse.from(this)
+                .choose(MimeType.of(MimeType.JPEG, MimeType.PNG, MimeType.WEBP), false)
+                .countable(true).capture(true).captureStrategy(CaptureStrategy(true,
+                    BuildConfig.APPLICATION_ID + ".fileprovider",
+                    "images")).gridExpectedSize(DensityUtils.dp2px(120f))
+                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT).thumbnailScale(0.85f)
+                .imageEngine(GlideEngine()).showSingleMediaType(true).originalEnable(true)
+                .maxOriginalSize(10).autoHideToolbarOnSingleTap(true)
+                .forResult { result: ActivityResult ->
+                    if (result.resultCode == AppCompatActivity.RESULT_OK && result.data != null) {
+                        val paths = Matisse.obtainPathResult(result.data)
+                        ToastUtils.show(paths)
+                    }
+                }
+        }
     }
 
     private fun showDialog2() {
