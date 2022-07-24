d3065366d5aa778bce73d7b70c75efa4fa1fad52
lany192 <lany192@qq.com>
2022-07-24 22:25:16 +0800

    update

--- arch/src/main/java/com/github/lany192/arch/utils/ListUtils.java
+++ arch/src/main/java/com/github/lany192/arch/utils/ListUtils.java
@@ -1,6 +1,7 @@
 package com.github.lany192.arch.utils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListUtils {
     /**
@@ -8,5 +9,26 @@ public class ListUtils {
      */
     public static boolean isEmpty(List<?> lists) {
         return lists == null || lists.size() == 0;
+    }
+
+    /**
+     * 逗号隔开拼接字符串,结果示例：  1,2,3,4
+     */
+    public static String list2string(List<Long> list) {
+        if (isEmpty(list)) {
+            return "";
+        }
+        List<String> items = list.stream().map(String::valueOf).collect(Collectors.toList());
+        return join(items);
+    }
+
+    /**
+     * 逗号隔开拼接字符串,结果示例：  a,b,c,d
+     */
+    public static String join(List<String> list) {
+        if (isEmpty(list)) {
+            return "";
+        }
+        return String.join(",", list);
     }
 }
