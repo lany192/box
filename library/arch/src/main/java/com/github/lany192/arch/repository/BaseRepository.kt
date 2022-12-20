cea4b744f45a27a2476d36bafc93f3105be0062e
lany192 <lany192@qq.com>
2022-12-20 17:38:32 +0800

    update

--- library/arch/src/main/java/com/github/lany192/arch/repository/BaseRepository.kt
+++ library/arch/src/main/java/com/github/lany192/arch/repository/BaseRepository.kt
@@ -5,10 +5,7 @@ import com.github.lany192.arch.entity.ApiResult
 import com.github.lany192.log.XLog
 import com.github.lany192.utils.NetUtils
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 
 open class BaseRepository {
     protected var log: XLog = XLog.tag(javaClass.name)
@@ -25,8 +22,11 @@ open class BaseRepository {
                 emit(ApiResult.network())
             }
         }.flowOn(Dispatchers.IO)
+            .onCompletion { exception ->
+                log.e("onCompletion：$exception")
+            }
             .catch {
-                log.e("请求异常:", it)
+                log.e("请求异常:${it.message}")
                 if (BuildConfig.DEBUG) {
                     emit(ApiResult.network(it.message))
                 } else {
