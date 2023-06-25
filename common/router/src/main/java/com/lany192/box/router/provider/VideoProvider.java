97b2620a1533e49484476412294a0104e7ab43f8
lany192 <lany192@qq.com>
2023-06-25 17:43:48 +0800

    update

---
+++ common/router/src/main/java/com/lany192/box/router/provider/VideoProvider.java
@@ -0,0 +1,8 @@
+package com.lany192.box.router.provider;
+
+import com.alibaba.android.arouter.facade.template.IProvider;
+
+public interface VideoProvider extends IProvider {
+
+    void startLogin();
+}
