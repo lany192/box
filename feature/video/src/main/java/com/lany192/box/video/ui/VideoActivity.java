9c3613a8e0be5ad070df1274553212236af0dde6
lany192 <lany192@qq.com>
2023-06-25 15:37:55 +0800

    update

---
+++ feature/video/src/main/java/com/lany192/box/video/ui/VideoActivity.java
@@ -0,0 +1,16 @@
+package com.lany192.box.video.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lany192.box.video.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
