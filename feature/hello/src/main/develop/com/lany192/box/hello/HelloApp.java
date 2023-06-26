bf427d2004809848ee2031cdd093131e6dde1826
lany192 <lany192@qq.com>
2023-06-26 10:51:48 +0800

    update

---
+++ feature/hello/src/main/develop/com/lany192/box/hello/HelloApp.java
@@ -0,0 +1,36 @@
+package com.lany192.box.hello;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.github.lany192.arch.BoxApplication;
+import com.jakewharton.processphoenix.ProcessPhoenix;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class HelloApp extends BoxApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (ProcessPhoenix.isPhoenixProcess(this)) {
+            return;
+        }
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        initARouter();
+    }
+
+    @Override
+    public boolean debug() {
+        return BuildConfig.DEBUG;
+    }
+
+    private void initARouter() {
+        if (BuildConfig.DEBUG) {
+            ARouter.openLog();
+            ARouter.openDebug();
+        }
+        ARouter.init(this);
+    }
+}
--- feature/hello/src/main/develop/com/lany192/box/hello/di/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.lany192.box.hello.di;
