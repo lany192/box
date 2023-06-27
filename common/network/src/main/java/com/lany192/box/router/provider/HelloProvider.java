216f1a0aa6fbd4adf25896223c6b9ce30bd1756c
lany192 <lany192@qq.com>
2023-06-27 11:42:08 +0800

    update

---
+++ common/network/src/main/java/com/lany192/box/router/provider/HelloProvider.java
@@ -0,0 +1,9 @@
+package com.lany192.box.router.provider;
+
+
+import com.alibaba.android.arouter.facade.template.IProvider;
+
+public interface HelloProvider extends IProvider {
+
+    void startHello();
+}
