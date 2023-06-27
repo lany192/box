67c885fcaa16b6d14d811dff9e13ae29293c8191
lany192 <lany192@qq.com>
2023-06-27 09:19:16 +0800

    update

--- feature/hello/src/main/java/com/lany192/box/hello/provider/HelloProviderImpl.java
+++ feature/hello/src/main/java/com/lany192/box/hello/provider/HelloProviderImpl.java
@@ -1,26 +1,23 @@
 package com.lany192.box.hello.provider;
 
 import android.content.Context;
-import android.content.Intent;
 
+import com.alibaba.android.arouter.HelloRouter;
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.lany192.box.hello.ui.HelloActivity;
 import com.lany192.box.router.provider.HelloProvider;
 
 
 @Route(path = "/hello/provider")
 public class HelloProviderImpl implements HelloProvider {
 
-    private Context context;
 
     @Override
     public void init(Context context) {
-        this.context = context;
     }
 
 
     @Override
     public void startHello() {
-        context.startActivity(new Intent(context, HelloActivity.class));
+        HelloRouter.startHello();
     }
 }
