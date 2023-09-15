ce2a818e70ced5ed6438695e8f968a2f5863271b
lany <lany192@qq.com>
2023-09-15 09:47:33 +0800

    update

--- library/core/src/main/java/com/github/lany192/extensions/ImageViews.kt
+++ library/core/src/main/java/com/github/lany192/extensions/ImageViews.kt
@@ -1,45 +1,15 @@
 package com.github.lany192.extensions
 
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.widget.ImageView
-import androidx.annotation.DrawableRes
+import com.bumptech.glide.request.RequestOptions
 import com.github.lany192.utils.ImageUtils
-
-import java.io.File
-
-@JvmSynthetic
-fun ImageView.load(uri: String?) {
-    ImageUtils.show(this, uri)
-}
-
-@JvmSynthetic
-fun ImageView.load(uri: Uri?) {
-    ImageUtils.show(this, uri)
-}
-
-@JvmSynthetic
-fun ImageView.load(file: File?) {
-    ImageUtils.show(this, file)
-}
-
-@JvmSynthetic
-fun ImageView.load(@DrawableRes drawableResId: Int) {
-    ImageUtils.show(this, drawableResId)
-}
-
-@JvmSynthetic
-fun ImageView.load(drawable: Drawable?) {
-    ImageUtils.show(this, drawable)
-}
-
-@JvmSynthetic
-fun ImageView.load(bitmap: Bitmap?) {
-    ImageUtils.show(this, bitmap)
-}
 
 @JvmSynthetic
 fun ImageView.load(data: Any?) {
     ImageUtils.show(this, data)
+}
+
+@JvmSynthetic
+fun ImageView.load(data: Any?, requestOptions: RequestOptions) {
+    ImageUtils.show(this, data, requestOptions)
 }
