ff40ca4ad8f28d486b1fa2e53213e064588aa1f7
lany <lany192@qq.com>
2023-12-23 15:49:26 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/test/TestActivity.java
+++ sample/src/main/java/com/lany192/box/sample/ui/test/TestActivity.java
@@ -1,6 +1,11 @@
 package com.lany192.box.sample.ui.test;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.github.lany192.arch.activity.ContentActivity;
@@ -15,5 +20,20 @@ public class TestActivity extends ContentActivity<ActivityTestBinding> {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding.button.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("*/*");
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivityForResult(Intent.createChooser(intent, "需要选择文件"), 1);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            binding.result.setText(uri.getPath());
+        }
     }
 }
