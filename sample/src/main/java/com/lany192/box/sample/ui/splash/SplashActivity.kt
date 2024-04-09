36555230b3ac3b1fff034ecb9c3a9dea4e2dd8de
lany <lany192@qq.com>
2024-04-09 10:53:14 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/splash/SplashActivity.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/splash/SplashActivity.kt
@@ -7,12 +7,16 @@ import android.os.Looper
 import com.alibaba.android.arouter.SampleRouter
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.github.lany192.arch.activity.BoxActivity
+import com.github.lany192.dialog.SimpleDialog
 import com.github.lany192.utils.ImageUtils
 import com.gyf.immersionbar.BarHide
 import com.gyf.immersionbar.ImmersionBar
 import com.lany192.box.sample.R
 import com.lany192.box.sample.databinding.ActivitySplashBinding
+import com.liulishuo.okdownload.OkDownloadProvider.context
+import com.scottyab.rootbeer.RootBeer
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 @Route(path = "/ui/splash")
@@ -35,9 +39,20 @@ class SplashActivity : BoxActivity<SplashViewModel, ActivitySplashBinding>() {
         }
         ImageUtils.show(binding.image, R.mipmap.demo)
         viewModel.welcome.observe(this) { s: String? -> binding.textView.text = s }
-        Handler(Looper.getMainLooper()).postDelayed({
-            SampleRouter.startMain()
-            finish()
-        }, 3000)
+        if (RootBeer(context).isRooted) {
+            val dialog = SimpleDialog()
+            dialog.setCanceledOnTouchOutside(false)
+            dialog.isCancelable = false
+            dialog.setMessage("检测到当前手机已经被root，存在数据不安全情况。为保证良好的用户体验，请选择在非root手机上使用本软件。")
+            dialog.setRightButton("退出") {
+                finish()
+            }
+            dialog.show()
+        } else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                SampleRouter.startMain()
+                finish()
+            }, 3000)
+        }
     }
 }
