ff27e5b7affb80e51cbbfc387e824643a89f2f41
lany <lany192@qq.com>
2024-04-11 14:00:23 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/main/menus/MenusAdapter.java
+++ sample/src/main/java/com/lany192/box/sample/ui/main/menus/MenusAdapter.java
@@ -3,12 +3,12 @@ package com.lany192.box.sample.ui.main.menus;
 import androidx.annotation.NonNull;
 
 import com.github.lany192.arch.adapter.BindingAdapter;
+import com.hjq.toast.Toaster;
 import com.lany192.box.sample.databinding.ItemMenuBinding;
 
 import java.util.List;
 
 public class MenusAdapter extends BindingAdapter<MenuItem, ItemMenuBinding> {
-
     public MenusAdapter(@NonNull List<MenuItem> data) {
         super(data);
     }
@@ -21,15 +21,35 @@ public class MenusAdapter extends BindingAdapter<MenuItem, ItemMenuBinding> {
     @Override
     public void onItemClicked(@NonNull ItemMenuBinding binding, MenuItem item, int position) {
         if (position == 0) {
-
+            test1();
         } else if (position == 1) {
-
+            test2();
         } else if (position == 2) {
-
+            test3();
         } else if (position == 3) {
-
+            test4();
         } else if (position == 4) {
-
+            test5();
         }
+    }
+
+    private void test1() {
+        Toaster.show("点击了1");
+    }
+
+    private void test2() {
+        Toaster.show("点击了1");
+    }
+
+    private void test3() {
+        Toaster.show("点击了1");
+    }
+
+    private void test4() {
+        Toaster.show("点击了1");
+    }
+
+    private void test5() {
+        Toaster.show("点击了1");
     }
 }
