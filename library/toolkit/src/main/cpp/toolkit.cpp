1efab79a7413a3f06a3e6eee881b44dc9c7d2f71
lany <lany192@qq.com>
2024-04-11 10:45:23 +0800

    update

--- library/toolkit/src/main/cpp/toolkit.cpp
+++ library/toolkit/src/main/cpp/toolkit.cpp
@@ -1,10 +1,32 @@
 #include <jni.h>
 #include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_github_lany192_toolkit_NativeLib_stringFromJNI(
+Java_com_github_lany192_toolkit_ToolKit_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jclass clazz) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_github_lany192_toolkit_ToolKit_getCurrentProcess(
+        JNIEnv* env,
+        jclass clazz) {
+    const std::string &path = "/proc/self/cmdline";
+    ifstream file;
+    file.open(path.c_str(), ios::in);
+    std::string res = "";
+    if (file.is_open()) {
+        string temp;
+        while (getline(file, temp)) {
+            res += temp;
+            res += '\n';
+        }
+    }
+    file.close();
+    return env->NewStringUTF(res.c_str());
 }
---
+++ library/toolkit/src/main/java/com/github/lany192/toolkit/ToolKit.java
@@ -0,0 +1,15 @@
+package com.github.lany192.toolkit;
+
+public class ToolKit {
+
+    static {
+        System.loadLibrary("toolkit");
+    }
+
+    public static native String stringFromJNI();
+
+    /**
+     * 不需要触发合规提示的获取进程名称方式
+     */
+    public static native String getCurrentProcess();
+}
