b6d2ceccff7218b6107d89cbdbc84f75ca58862e
lany <lany192@qq.com>
2024-04-18 10:44:20 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/data/binder/AreaBinder.kt
+++ sample/src/main/java/com/lany192/box/sample/data/binder/AreaBinder.kt
@@ -1,33 +1,29 @@
-package com.lany192.box.sample.data.binder;
+package com.lany192.box.sample.data.binder
 
-import androidx.annotation.NonNull;
+import com.github.lany192.arch.items.ItemBinder
+import com.github.lany192.dialog.SimpleDialog
+import com.github.lany192.extensions.load
+import com.hjq.toast.Toaster
+import com.lany192.box.network.data.bean.Area
+import com.lany192.box.sample.MockUtils
+import com.lany192.box.sample.databinding.ItemAreaBinding
 
-import com.github.lany192.arch.items.ItemBinder;
-import com.github.lany192.dialog.SimpleDialog;
-import com.github.lany192.utils.ImageUtils;
-import com.hjq.toast.Toaster;
-import com.lany192.box.network.data.bean.Area;
-import com.lany192.box.sample.MockUtils;
-import com.lany192.box.sample.databinding.ItemAreaBinding;
+class AreaBinder : ItemBinder<Area, ItemAreaBinding>() {
 
-public class AreaBinder extends ItemBinder<Area, ItemAreaBinding> {
-
-    @Override
-    public void onItemClick(@NonNull ItemAreaBinding holder, Area area, int position) {
-        int count = area.getSubarea() != null ? area.getSubarea().size() : 0;
-        SimpleDialog dialog = new SimpleDialog();
-        dialog.setTitle("提示");
-        dialog.setMessage(area.getName() + count + "个地级市");
-        dialog.setRightButton("确定", () -> Toaster.show(area.getName()));
-        dialog.setLeftButton("取消");
-        dialog.show();
+    override fun onItemClick(binding: ItemAreaBinding, item: Area, position: Int) {
+        val count = if (item.subarea != null) item.subarea.size else 0
+        val dialog = SimpleDialog()
+        dialog.setTitle("提示")
+        dialog.setMessage(item.name + count + "个地级市")
+        dialog.setRightButton("确定") { Toaster.show(item.name) }
+        dialog.setLeftButton("取消")
+        dialog.show()
     }
 
-    @Override
-    public void convert(@NonNull ItemAreaBinding binding, Area item, int position) {
-        int count = item.getSubarea() != null ? item.getSubarea().size() : 0;
-        ImageUtils.show(binding.image, MockUtils.getImageUrl());
-        binding.title.setText(item.getName());
-        binding.desc.setText("下辖" + count + "个区/市");
+    override fun convert(binding: ItemAreaBinding, item: Area, position: Int) {
+        val count = if (item.subarea != null) item.subarea.size else 0
+        binding.image.load(MockUtils.getImageUrl())
+        binding.title.text = item.name
+        binding.desc.text = "下辖" + count + "个区/市"
     }
 }
