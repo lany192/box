1d8a43fee1902fb4400dfce84314c9a2d2bd0dff
lany <lany192@qq.com>
2024-05-10 16:37:23 +0800

    update

---
+++ feature/math/src/main/java/com/lany192/box/math/ui/division/DivisionActivity.java
@@ -0,0 +1,31 @@
+package com.lany192.box.math.ui.division;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.lany192.box.math.R;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+@Route(path = "/math/division")
+public class DivisionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_division);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
