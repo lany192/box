add612b880189cd49f0fa0aef1d6a71d959b42ee
lany <lany192@qq.com>
2024-05-14 14:55:49 +0800

    update

---
+++ library/core/src/main/java/com/github/lany192/log/LogUtils.java
@@ -0,0 +1,95 @@
+package com.github.lany192.log;
+
+import android.content.Context;
+
+import com.tencent.mars.xlog.Log;
+import com.tencent.mars.xlog.Xlog;
+
+public class LogUtils {
+    /**
+     * 初始化XLog
+     */
+    public static void init(Context context, boolean debug) {
+        //加载必要so
+        System.loadLibrary("c++_shared");
+        System.loadLibrary("marsxlog");
+
+        //日志储存路径
+        final String logPath = context.getFilesDir() + "/xlog";
+        //日志文件前缀
+        final String namePrefix = context.getClass().getSimpleName() + "_box";
+        //缓存天数
+        final int cacheDays = 7;
+        //日志等级
+        int level = debug ? Xlog.LEVEL_VERBOSE : Xlog.LEVEL_INFO;
+
+        //init xlog
+        Xlog xlog = new Xlog();
+        Log.setLogImp(xlog);
+
+        //是否控制台打印
+        Log.setConsoleLogOpen(debug);
+
+        Log.appenderOpen(level, Xlog.AppednerModeAsync, "", logPath, namePrefix, cacheDays);
+
+        Log.i("Xlog", "日志debug:" + debug);
+        Log.i("Xlog", "日志储存路径:" + logPath);
+    }
+
+    public static XLog tag(String tag) {
+        return new XLog(tag);
+    }
+
+    public static void close() {
+        Log.i("Xlog", "日志close()");
+        Log.appenderClose();
+    }
+
+    public static void f(String msg) {
+        Log.f("Xlog", msg);
+    }
+
+    public static void e(String msg) {
+        Log.e("Xlog", msg);
+    }
+
+    public static void w(String msg) {
+        Log.w("Xlog", msg);
+    }
+
+    public static void i(String msg) {
+        Log.i("Xlog", msg);
+    }
+
+    public static void d(String msg) {
+        Log.d("Xlog", msg);
+    }
+
+    public static void v(String msg) {
+        Log.v("Xlog", msg);
+    }
+
+    public static void f(String format, Object... obj) {
+        Log.f("Xlog", format, obj);
+    }
+
+    public static void e(String format, Object... obj) {
+        Log.e("Xlog", format, obj);
+    }
+
+    public static void w(String format, Object... obj) {
+        Log.w("Xlog", format, obj);
+    }
+
+    public static void i(String format, Object... obj) {
+        Log.i("Xlog", format, obj);
+    }
+
+    public static void d(String format, Object... obj) {
+        Log.d("Xlog", format, obj);
+    }
+
+    public static void v(String format, Object... obj) {
+        Log.v("Xlog", format, obj);
+    }
+}
---
+++ library/core/src/main/java/com/github/lany192/log/XLog.java
@@ -0,0 +1,98 @@
+package com.github.lany192.log;
+
+import com.tencent.mars.xlog.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class XLog {
+    private final String tag;
+
+    public XLog(String tag) {
+        this.tag = tag;
+    }
+
+    public static XLog tag(String tag) {
+        return new XLog(tag);
+    }
+
+    /**
+     * 格式化json
+     */
+    private String format(String json) {
+        String formattedString;
+        if (json == null || json.trim().length() == 0) {
+            w("JSON empty.");
+            return "";
+        }
+        final int JSON_INDENT = 4;
+        try {
+            if (json.startsWith("{")) {
+                JSONObject jsonObject = new JSONObject(json);
+                formattedString = jsonObject.toString(JSON_INDENT);
+            } else if (json.startsWith("[")) {
+                JSONArray jsonArray = new JSONArray(json);
+                formattedString = jsonArray.toString(JSON_INDENT);
+            } else {
+                w("JSON should start with { or [");
+                return json;
+            }
+        } catch (Exception e) {
+            e(e.getMessage());
+            return json;
+        }
+        return " \n" + formattedString;
+    }
+
+    public void json(String json) {
+        Log.i(tag, format(json));
+    }
+
+    public void f(String msg) {
+        Log.f(tag, msg);
+    }
+
+    public void e(String msg) {
+        Log.e(tag, msg);
+    }
+
+    public void w(String msg) {
+        Log.w(tag, msg);
+    }
+
+    public void i(String msg) {
+        Log.i(tag, msg);
+    }
+
+    public void d(String msg) {
+        Log.d(tag, msg);
+    }
+
+    public void v(String msg) {
+        Log.v(tag, msg);
+    }
+
+    public void f(String format, Object... obj) {
+        Log.f(tag, format, obj);
+    }
+
+    public void e(String format, Object... obj) {
+        Log.e(tag, format, obj);
+    }
+
+    public void w(String format, Object... obj) {
+        Log.w(tag, format, obj);
+    }
+
+    public void i(String format, Object... obj) {
+        Log.i(tag, format, obj);
+    }
+
+    public void d(String format, Object... obj) {
+        Log.d(tag, format, obj);
+    }
+
+    public void v(String format, Object... obj) {
+        Log.v(tag, format, obj);
+    }
+}
