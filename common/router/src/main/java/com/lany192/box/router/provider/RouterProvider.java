6ffd6281ed4b5b8d6a05d8ad9192e5bc5765ccde
lany <lany192@qq.com>
2024-05-20 16:27:40 +0800

    update

--- common/router/src/main/java/com/lany192/box/router/provider/RouterProvider.java
+++
@@ -1,5 +0,0 @@
-package com.lany192.box.router.provider;
-
-public interface RouterProvider extends BrowserProvider, MathProvider, LoginProvider, HelloProvider {
-
-}
--- feature/hello/src/main/java/com/lany192/box/hello/ui/HelloActivity.java
+++ feature/hello/src/main/java/com/lany192/box/hello/ui/HelloActivity.java
@@ -1,21 +1,31 @@
 package com.lany192.box.hello.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.lany192.box.hello.R;
+import com.lany192.box.hello.databinding.ActivityHelloBinding;
+import com.lany192.box.router.provider.BrowserProvider;
+
+import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 @Route(path = "/hello/main")
 public class HelloActivity extends AppCompatActivity {
+    @Autowired
+    BrowserProvider browserProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
+        ActivityHelloBinding binding = ActivityHelloBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.test1.setOnClickListener(v -> browserProvider.startBrowser("测试", "https://www.baidu.com"));
     }
 }
