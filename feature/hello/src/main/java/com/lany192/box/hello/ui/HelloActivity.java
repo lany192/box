bbcf7e0ec01fe353dfcacfff8bf757f8fb2bb53d
lany <lany192@qq.com>
2024-05-20 16:29:39 +0800

    update

--- feature/hello/src/main/java/com/lany192/box/hello/ui/HelloActivity.java
+++ feature/hello/src/main/java/com/lany192/box/hello/ui/HelloActivity.java
@@ -10,6 +10,7 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.lany192.box.hello.R;
 import com.lany192.box.hello.databinding.ActivityHelloBinding;
 import com.lany192.box.router.provider.BrowserProvider;
+import com.lany192.box.router.provider.MathProvider;
 
 import javax.inject.Inject;
 
@@ -20,12 +21,14 @@ import dagger.hilt.android.AndroidEntryPoint;
 public class HelloActivity extends AppCompatActivity {
     @Autowired
     BrowserProvider browserProvider;
-
+    @Autowired
+    MathProvider mathProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityHelloBinding binding = ActivityHelloBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.test1.setOnClickListener(v -> browserProvider.startBrowser("测试", "https://www.baidu.com"));
+        binding.test2.setOnClickListener(v -> mathProvider.startMath());
     }
 }
