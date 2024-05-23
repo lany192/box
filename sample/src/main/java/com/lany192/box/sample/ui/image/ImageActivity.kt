7eb4a9a12b59dfac4047130568e74cd8e44bf294
lany <lany192@qq.com>
2024-05-23 14:25:14 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/image/ImageActivity.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/image/ImageActivity.kt
@@ -17,10 +17,10 @@ class ImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_image)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
