3604e824d433dd8a2135a3ed14c3f1d04dbfeb61
lany <lany192@qq.com>
2024-05-23 14:37:24 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/image/ImageActivity.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/image/ImageActivity.kt
@@ -1,25 +1,27 @@
 package com.lany192.box.sample.ui.image
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.lany192.box.sample.R
+import com.lany192.box.sample.databinding.ActivityImageBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 @Route(path = "/ui/image")
 class ImageActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_image)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val binding = ActivityImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            Log.i("高度：", "结果：" + systemBars.top + "，结果：" + systemBars.bottom)
             insets
         }
     }
