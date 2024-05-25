ad71eb2fb8f906d7ed75df699bf2006f757944a7
lany <lany192@qq.com>
2024-05-25 11:52:42 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/database/DatabaseActivity.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/database/DatabaseActivity.kt
@@ -1,16 +1,10 @@
 package com.lany192.box.sample.ui.database
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.github.lany192.arch.activity.ViewBindingActivity
 import com.github.lany192.arch.activity.ViewModelActivity
-import com.github.lany192.arch.utils.ListUtils
 import com.gyf.immersionbar.ImmersionBar
-import com.lany192.box.sample.R
 import com.lany192.box.sample.databinding.ActivityDatabaseBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
