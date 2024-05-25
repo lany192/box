69f2ef663af3a32657f11283fc7c653c84504344
lany <lany192@qq.com>
2024-05-25 10:23:02 +0800

    update

--- common/database/src/main/java/com/lany192/box/database/AppDatabase.java
+++ common/database/src/main/java/com/lany192/box/database/AppDatabase.java
@@ -4,8 +4,8 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-import com.lany192.box.database.Converters;
 import com.lany192.box.database.entity.SearchHistory;
+import com.lany192.box.database.mapper.BrowseHistoryMapper;
 import com.lany192.box.database.mapper.SearchHistoryMapper;
 
 
@@ -16,4 +16,9 @@ public abstract class AppDatabase extends RoomDatabase {
      * 搜索记录
      */
     public abstract SearchHistoryMapper searchHistoryMapper();
+
+    /**
+     * 浏览记录
+     */
+    public abstract BrowseHistoryMapper browseHistoryMapper();
 }
--- common/database/src/main/java/com/lany192/box/database/repository/DatabaseRepository.kt
+++ common/database/src/main/java/com/lany192/box/database/repository/DatabaseRepository.kt
@@ -2,6 +2,7 @@ package com.lany192.box.database.repository
 
 import com.github.lany192.arch.repository.BaseRepository
 import com.lany192.box.database.AppDatabase
+import com.lany192.box.database.entity.BrowseHistory
 import com.lany192.box.database.entity.SearchHistory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -41,6 +42,21 @@ class DatabaseRepository(private val database: AppDatabase) : BaseRepository() {
             exception.printStackTrace()
         }.collect {
             log.i("搜索历史记录插入成功：$it")
+        }
+    }
+
+    /**
+     * 保存搜索记录
+     */
+    suspend fun saveBrowseHistory(record: BrowseHistory) {
+        flow {
+            val result = database.browseHistoryMapper().insertRecord(record)
+            emit(result)
+        }.flowOn(Dispatchers.IO).catch { exception ->
+            log.e(exception.message)
+            exception.printStackTrace()
+        }.collect {
+            log.i("搜索记录插入成功：$it")
         }
     }
 }
