5ed75d4844c6bd6cc1e8c7d0a4a54fbde05220d9
lany192 <lany192@qq.com>
2024-06-02 21:05:57 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/blur/BlurActivity.kt
+++ sample/src/main/java/com/lany192/box/sample/ui/blur/BlurActivity.kt
@@ -1,12 +1,15 @@
 package com.lany192.box.sample.ui.blur
 
+import android.graphics.Outline
 import android.os.Bundle
+import android.view.View
+import android.view.ViewOutlineProvider
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.lany192.box.sample.R
+import com.lany192.box.sample.databinding.ActivityBlurBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,11 +19,22 @@ class BlurActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_blur)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val binding = ActivityBlurBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.blurView.setupWith(binding.root)
+            .setFrameClearDrawable(window.decorView.background)
+            .setBlurRadius(15f)
+        binding.blurView.setClipToOutline(true)
+        binding.blurView.outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                binding.blurView.background.getOutline(outline)
+                outline.alpha = 1f
+            }
         }
     }
 }
