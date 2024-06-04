1e683ef87a6b07ae8135b93a2841a050bbe0ab5d
lany <lany192@qq.com>
2024-06-04 16:19:56 +0800

    update

--- sample/src/main/java/com/lany192/box/sample/ui/html/HtmlActivity.java
+++ sample/src/main/java/com/lany192/box/sample/ui/html/HtmlActivity.java
@@ -3,9 +3,7 @@ package com.lany192.box.sample.ui.html;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -13,11 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.lany192.box.sample.R;
 
+import net.nightwhistler.htmlspanner.HtmlSpanner;
+
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 @Route(path = "/ui/html")
 public class HtmlActivity extends AppCompatActivity {
+    HtmlSpanner htmlSpanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +30,19 @@ public class HtmlActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("测试html");
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        htmlSpanner = new HtmlSpanner();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                final Spannable spannable = htmlSpanner.fromHtml(html);
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        tv.setText(spannable);
+//                        tv.setMovementMethod(LinkMovementMethodExt.getInstance(handler, ImageSpan.class));
+//                    }
+//                });
+//            }
+//        }).start();
     }
 }
