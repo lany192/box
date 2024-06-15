98ff18cf5a35c8102a3ce93e706d310c5adbdfd3
lany <lany192@qq.com>
2024-06-15 14:12:13 +0800

    update

--- feature/browser/src/main/java/com/lany192/box/browser/ui/BrowserActivity.java
+++ feature/browser/src/main/java/com/lany192/box/browser/ui/BrowserActivity.java
@@ -1,6 +1,7 @@
 package com.lany192.box.browser.ui;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -29,5 +30,17 @@ public class BrowserActivity extends ViewBindingActivity<ActivityBrowserBinding>
         setTitle(title);
         binding.webview.loadUrl(url);
         log.i("标题:" + title + ",链接:" + url);
+    }
+
+    /**
+     * javascript回调
+     *
+     * @param callback 回调方法
+     * @param value    回调值
+     */
+    private void callback(String callback, String value) {
+        if (!TextUtils.isEmpty(callback) && !TextUtils.isEmpty(callback.trim())) {
+            binding.webview.loadUrl("javascript:" + callback + "('" + value + "')");
+        }
     }
 }
