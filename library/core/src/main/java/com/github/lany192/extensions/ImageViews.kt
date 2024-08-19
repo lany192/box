3e220b767e6ad879c52588ab4410c988d704b4c0
lany <lany192@qq.com>
2024-08-19 17:31:19 +0800

    update

--- library/core/src/main/java/com/github/lany192/extensions/ImageViews.kt
+++ library/core/src/main/java/com/github/lany192/extensions/ImageViews.kt
@@ -4,12 +4,6 @@ import android.widget.ImageView
 import com.bumptech.glide.request.RequestOptions
 import com.github.lany192.utils.ImageUtils
 
-@JvmSynthetic
-fun ImageView.load(data: Any?) {
-    ImageUtils.show(this, data)
-}
-
-@JvmSynthetic
-fun ImageView.load(data: Any?, requestOptions: RequestOptions) {
+fun ImageView.load(data: Any?, requestOptions: RequestOptions = RequestOptions()) {
     ImageUtils.show(this, data, requestOptions)
 }
