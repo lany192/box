1558128a59a8896ca26ba8a318b0f70a552d2986
lany <lany192@qq.com>
2024-09-06 09:35:07 +0800

    update

--- library/arch/src/main/java/com/github/lany192/arch/eventbus/util/ILogger.kt
+++
@@ -1,8 +0,0 @@
-package com.github.lany192.arch.eventbus.util
-
-import java.util.logging.Level
-
-interface ILogger {
-    fun log(level: Level, msg: String)
-    fun log(level: Level, msg: String, th: Throwable)
-}
