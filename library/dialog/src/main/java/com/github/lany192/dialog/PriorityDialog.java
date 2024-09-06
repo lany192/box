f569b34fa527a400c9f1e61752239a58784660c3
lany <lany192@qq.com>
2024-09-06 11:45:55 +0800

    update

--- library/dialog/src/main/java/com/github/lany192/dialog/PriorityDialog.java
+++ library/dialog/src/main/java/com/github/lany192/dialog/PriorityDialog.java
@@ -283,6 +283,10 @@ public abstract class PriorityDialog extends DialogFragment implements Comparabl
         if (window != null) {
             window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             window.setDimAmount(getAmount());
+            if (bottomStyle()) {
+                //处理导航栏区域
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
         }
         return dialog;
     }
