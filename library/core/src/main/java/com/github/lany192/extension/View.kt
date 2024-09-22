9036842ba57cb291137633da99ab82ba57fb9094
lany192 <lany192@qq.com>
2024-09-22 19:09:17 +0800

    update

--- library/core/src/main/java/com/github/lany192/extension/View.kt
+++ library/core/src/main/java/com/github/lany192/extension/View.kt
@@ -1,9 +1,32 @@
 package com.github.lany192.extension
 
+import android.graphics.Rect
+import android.view.TouchDelegate
 import android.view.View
 import android.view.View.OnClickListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+/**
+ * 扩展方法，扩大点击区域
+ * NOTE: 需要保证目标targetView有父View，否则无法扩大点击区域
+ *
+ * @param expandSize 扩大的大小，单位px
+ */
+fun View.expandTouchView(expandSize: Int = 10.dp.toInt()) {
+    val parentView = (parent as? View)
+    parentView?.post {
+        val rect = Rect()
+        getHitRect(rect) //getHitRect(rect)将视图在父容器中所占据的区域存储到rect中。
+        log("rect = $rect")
+        rect.left -= expandSize
+        rect.top -= expandSize
+        rect.right += expandSize
+        rect.bottom += expandSize
+        log("expandRect = $rect")
+        parentView.touchDelegate = TouchDelegate(rect, this)
+    }
+}
 
 /**
  * 防止重复点击
