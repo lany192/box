f17a5002228598a02318ca538a78a953686420f1
lany192 <lany192@qq.com>
2024-11-01 21:53:03 +0800

    update

--- sample/avatar/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_lany192_box_avatar_AvatarApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.lany192.box.avatar.AvatarApp"
-)
-public final class _com_lany192_box_avatar_AvatarApp {
-}
