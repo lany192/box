2333534fd6403d3f2ea1cadd89694027994733d9
lany <lany192@qq.com>
2025-01-10 14:37:10 +0800

    build: 更新插件名称和版本
    - 修改 Google KSP 和 Hilt Android 插件的名称- 更新插件版本引用
    - 调整部分构建配置以适应新插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.android.hilt) apply false
+    alias(libs.plugins.android.ksp) apply false
 }
 
 allprojects {
---
+++ common/database/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.android.ksp)
+    alias(libs.plugins.android.hilt)
+}
+
+android {
+    namespace = libs.versions.app.id.get() + "." + project.name
+    compileSdk = libs.versions.app.compile.sdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.app.min.sdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    lint {
+        targetSdk = libs.versions.app.target.sdk.get().toInt()
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar", "*.aar"), "dir" to "libs")))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit2)
+    androidTestImplementation(libs.androidx.test.espresso)
+
+    implementation(project(":library:arch"))
+    implementation(project(":library:view"))
+    implementation(project(":library:core"))
+    implementation(project(":library:dialog"))
+    implementation(project(":library:time"))
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+}
