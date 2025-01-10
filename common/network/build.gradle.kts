b73875b631fb1f06fde97f450eefd1b95a124dd8
lany <lany192@qq.com>
2025-01-10 14:50:28 +0800

    build:将 network 和 router模块的 build.gradle 重命名为 build.gradle.kts
    - 将 common/network/build.gradle 重命名为 common/network/build.gradle.kts
    -将 common/router/build.gradle 重命名为 common/router/build.gradle.kts
    - 更新 build.gradle.kts 文件中的配置，使用 Kotlin DSL 语法

---
+++ common/network/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.android.hilt)
+}
+
+android {
+    namespace = libs.versions.app.id.get() + "." + project.name
+    compileSdk = libs.versions.app.compile.sdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.app.min.sdk.get().toInt()
+        targetSdk = libs.versions.app.target.sdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar", "*.aar"), "dir" to "libs")))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit2)
+    androidTestImplementation(libs.androidx.test.espresso)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.retrofit.gson)
+
+    implementation(libs.moshi)
+}
---
+++ common/router/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = libs.versions.app.id.get() + "." + project.name
+    compileSdk = libs.versions.app.compile.sdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.app.min.sdk.get().toInt()
+        targetSdk = libs.versions.app.target.sdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar", "*.aar"), "dir" to "libs")))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit2)
+    androidTestImplementation(libs.androidx.test.espresso)
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.lany.router)
+
+    implementation(project(":library:core"))
+}
